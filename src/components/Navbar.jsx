5021784d4dac1c96d34f16d820d40a9bba892c41
abdurrahmansoftlinkbd <kids.hasib.softlinkbd@gmail.com>
2025-06-27 22:34:43 +0600

    added navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 const Navbar = () => {
+  const menuItems = ["about", "projects", "skills", "contact"];
+
   return (
-    <div className="navbar bg-base-100 shadow-sm">
+    <div className="navbar border-b-2 border-b-[#e0e0e0]">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -24,53 +26,26 @@ const Navbar = () => {
             tabIndex={0}
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
           >
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
+            {menuItems.map((item) => (
+              <li key={item}>
+                <a className="capitalize">{item}</a>
+              </li>
+            ))}
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">daisyUI</a>
       </div>
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </details>
-          </li>
-          <li>
-            <a>Item 3</a>
-          </li>
+        <ul className="menu menu-horizontal px-1 gap-5">
+          {menuItems.map((item) => (
+            <li key={item}>
+              <a className="capitalize hover:bg-[#f5f5f5] py-2 px-4 rounded-3xl">
+                {item}
+              </a>
+            </li>
+          ))}
         </ul>
       </div>
-      <div className="navbar-end">
-        <a className="btn">Button</a>
-      </div>
+      <div className="navbar-end"></div>
     </div>
   );
 };
