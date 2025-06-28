500a111bafbf6a0fe3a4c1bda62e0a1dbe412d75
abdurrahmansoftlinkbd <kids.hasib.softlinkbd@gmail.com>
2025-06-28 09:23:46 +0600

    added typewriter effect in the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,28 @@
 import { FaFacebook, FaGithub, FaLinkedin } from "react-icons/fa";
 import { FiDownload } from "react-icons/fi";
+import Typewriter from "typewriter-effect";
 
 const Hero = () => {
   return (
-    <header className="hero font-roboto">
+    <header className="hero min-h-screen font-roboto">
       <div className="hero-content text-center">
         <div className="max-w-lg">
           <h2 className="text-5xl font-bold text-zinc-950">I'm Shakil Ahmed</h2>
-          <p className="text-xl font-medium mt-6">
-            Google Merchant Center Expert
-          </p>
+          <div className="text-xl font-medium mt-6">
+            <Typewriter
+              options={{
+                strings: [
+                  "Google Merchant Center Expert",
+                  "Google Shopping Ads Specialist",
+                  "Google Ads Product Feed Expert",
+                ],
+                autoStart: true,
+                loop: true,
+                delay: 75,
+                deleteSpeed: 50,
+              }}
+            />
+          </div>
           <p className="py-6 flex gap-4 justify-center items-center">
             <a href="https://github.com" target="_blank">
               <FaGithub className="text-2xl cursor-pointer" />
